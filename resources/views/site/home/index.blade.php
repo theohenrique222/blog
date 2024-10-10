260d3cf094f84407ed218678c178f3d4ce12d41b
Theo Henrique <theodoro222@hotmail.com>
2024-10-10 14:14:20 -0300

    integração do tailwind css

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -2,11 +2,11 @@
 <html lang="pt_BR">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> @vite('resources/css/app.css')
     <title>Blog</title>
 </head>
 <body>
-    <h1>
+    <h1 class="text-red-500">
         View referente a nossa página Home {{ \Carbon\Carbon::now()->format('d/m/Y H:i:s') }}
     </h1>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class);
 
 Route::get('/enviar', [EnviarSMSController::class, 'enviarSMS'])->name('enviar-sms.index');
+
